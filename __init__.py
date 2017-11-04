92fcef41e3f1106241551275fdf106b354d02a62
Andy Ganse <andy@ganse.org>
2017-11-04 00:35:55 -0700

    initial commit

--- __init__.py
+++ __init__.py
@@ -5,17 +5,3 @@ from ag_implement.skl_rf import skl_rf, create_tree_plots, plotROC, plotFI
 # from ag_implement import use_model, summarize_regrmodels, Keras_MLP as mlp
 # or
 # from ag_implement import skl_rf, create_tree_plots, plotROC, plotFI
-
-
-# My use with SPutils (in Jupyter cells):
-# %autoreload
-# from ag_implement import use_model, summarize_regrmodels, Keras_MLP as mlp
-#
-# results=[]
-# metrics,y_testpred,y_trainpred = use_model(mlp(lr=0.0025, epochs=100, hlay=[20,20,20], alpha=0.),
-#                                             X_train, y_train, X_test, y_test)
-# results.append(metrics)
-#
-# [some other use_model() and results.append() runs with other models...]
-#
-# summarize_regrmodels(results)
